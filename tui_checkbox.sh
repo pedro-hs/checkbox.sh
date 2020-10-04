8ab4e1c70c8285fd204b3d324c8364c1b8b2e168
pedrosantosarruda <pedro.arruda@pisomtech.com.br>
2020-10-04 01:50:52 -0300

    Organize script in sections

--- tui_checkbox.sh
+++ tui_checkbox.sh
@@ -22,6 +22,9 @@ BLUE="\e[2K\e[34m"
 RED="\e[2K\e[31m"
 GREEN="\e[2K\e[32m"
 
+#===============================================================================
+# VARS
+#===============================================================================
 options=("Option 1" "Option 2" "Option 3" "Option 4" "Option 5" "Option 6" "Option 7" "Option 8" "Option 9" "Option 10" "Option 11" "Option 12" "Option 13" "Option 14" "Option 15" "Option 16" "Option 17" "Option 18" "Option 19" "Option 20" "Option 21" "Option 22" "Option 23" "Option 24" "Option 25" "Option 26" "Option 27" "Option 28" "Option 29" "Option 30")
 
 cursor=0
@@ -37,6 +40,9 @@ selected_options=()
 
 content=""
 color=$WHITE
+
+#===============================================================================
+# UTILS
 #===============================================================================
 array_contains_value() {
     local e match="$1"
@@ -58,6 +64,9 @@ array_without_value() {
     echo "${args[@]}"
 }
 
+#===============================================================================
+# RENDERIZATION
+#===============================================================================
 index_in_page() {
     lines_amount=$(tput lines)
     options_length=${#options[@]}
@@ -108,6 +117,7 @@ set_line_color() {
 draw() {
     render
     content=""
+
     for index in "${!options[@]}"; do
         if index_in_page $index; then
             option=${options[$index]}
@@ -122,6 +132,7 @@ draw() {
             fi
         fi
     done
+
     render
 }
 
@@ -130,31 +141,52 @@ render() {
     echo -en "${content}"
 }
 
-handle_key_press() {
-    IFS= read -sN1 key 2>/dev/null >&2
 
-    read -sN1 -t 0.0001 k1;
-    read -sN1 -t 0.0001 k2;
-    read -sN1 -t 0.0001 k3;
-    key+=${k1}${k2}${k3}
+#===============================================================================
+# KEY ACTIONS
+#===============================================================================
+toggle_select_mode() {
+    if $multiple_options; then
+        unselect_mode=false
 
-    case "${key}" in
-        '') key=_enter;;
-        ' ') key=_space;;
-        $'\x1b') key=_esc;;
-        $'\x1b\x5b\x36\x7e') key=_pgdown;;
-        $'\x1b\x5b\x35\x7e') key=_pgup;;
-        $'\x7f') key=_backspace;;
-        $'\x1b\x5b\x32\x7e') key=_insert;;
-        $'\e[A'|$'\e0A  '|$'\e[D'|$'\e0D') key=_up;;
-        $'\e[B'|$'\e0B'|$'\e[C'|$'\e0C') key=_down;;
-        $'\e[1~'|$'\e0H'|$'\e[H') key=_home;;
-        $'\e[4~'|$'\e0F'|$'\e[F') key=_end;;
-        $'\e') key=_enter;;
-        $'\x0a') key=_enter;;
-    esac
+        if $select_mode; then
+            select_mode=false
 
-    echo $key
+        else
+            select_mode=true
+            selected_options+=("$cursor")
+        fi
+    fi
+}
+
+toggle_unselect_mode() {
+    if $multiple_options; then
+        select_mode=false
+
+        if $unselect_mode; then
+            unselect_mode=false
+
+        else
+            unselect_mode=true
+            selected_options=($(array_without_value "$cursor" "${selected_options[@]}"))
+        fi
+    fi
+}
+
+select_all() {
+    if $multiple_options; then
+        for index in ${!options[@]}; do
+            selected_options+=("${index}")
+        done
+    fi
+}
+
+unselect_all() {
+    if $multiple_options; then
+        for index in ${!options[@]}; do
+            selected_options=($(array_without_value "$index" "${selected_options[@]}"))
+        done
+    fi
 }
 
 page_up() {
@@ -211,22 +243,9 @@ confirm() {
     export output
 }
 
-select_all() {
-    if $multiple_options; then
-        for index in ${!options[@]}; do
-            selected_options+=("${index}")
-        done
-    fi
-}
-
-unselect_all() {
-    if $multiple_options; then
-        for index in ${!options[@]}; do
-            selected_options=($(array_without_value "$index" "${selected_options[@]}"))
-        done
-    fi
-}
-
+#===============================================================================
+# HANDLERS
+#===============================================================================
 handle_parameters() {
     while (( "$#" )); do
         opt="${1}"
@@ -239,34 +258,36 @@ handle_parameters() {
     done
 }
 
-toggle_select_mode() {
-    if $multiple_options; then
-        unselect_mode=false
+handle_key_press() {
+    IFS= read -sN1 key 2>/dev/null >&2
 
-        if $select_mode; then
-            select_mode=false
+    read -sN1 -t 0.0001 k1;
+    read -sN1 -t 0.0001 k2;
+    read -sN1 -t 0.0001 k3;
+    key+=${k1}${k2}${k3}
 
-        else
-            select_mode=true
-            selected_options+=("$cursor")
-        fi
-    fi
+    case "${key}" in
+        '') key=_enter;;
+        ' ') key=_space;;
+        $'\x1b') key=_esc;;
+        $'\x1b\x5b\x36\x7e') key=_pgdown;;
+        $'\x1b\x5b\x35\x7e') key=_pgup;;
+        $'\x7f') key=_backspace;;
+        $'\x1b\x5b\x32\x7e') key=_insert;;
+        $'\e[A'|$'\e0A  '|$'\e[D'|$'\e0D') key=_up;;
+        $'\e[B'|$'\e0B'|$'\e[C'|$'\e0C') key=_down;;
+        $'\e[1~'|$'\e0H'|$'\e[H') key=_home;;
+        $'\e[4~'|$'\e0F'|$'\e[F') key=_end;;
+        $'\e') key=_enter;;
+        $'\x0a') key=_enter;;
+    esac
+
+    echo $key
 }
 
-toggle_unselect_mode() {
-    if $multiple_options; then
-        select_mode=false
-
-        if $unselect_mode; then
-            unselect_mode=false
-
-        else
-            unselect_mode=true
-            selected_options=($(array_without_value "$cursor" "${selected_options[@]}"))
-        fi
-    fi
-}
-
+#===============================================================================
+# MAIN
+#===============================================================================
 main() {
     handle_parameters $1 $2
     draw
@@ -287,6 +308,7 @@ main() {
             _insert|v) toggle_select_mode;;
             _backspace|V) toggle_unselect_mode;;
             r) render;;
+            a) select_all;;
             A) unselect_all;;
         esac
 
